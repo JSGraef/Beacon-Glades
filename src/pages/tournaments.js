6d41bb72873a8893321e8e44d60eeb48aecf40c7
jsgraef <jg@joshgraef.com>
2021-01-19 20:39:21 -0500

    Add tournament page

--- src/pages/tournaments.js
+++ src/pages/tournaments.js
@@ -4,7 +4,25 @@ import Layout from "../Layout.js"
 export default function Tournaments() {
   return (
     <Layout>
-      <div>Tournaments</div>
+      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+        <div className="lg:grid lg:grid-cols-3 lg:gap-8">
+          <div>
+            <h2 className="text-3xl font-extrabold text-gray-900">
+              Tournaments
+            </h2>
+
+          </div>
+          <div className="mt-12 lg:mt-0 lg:col-span-2">
+            <dl className="space-y-12">
+              <div>
+                <dt className="text-lg leading-6 font-medium text-gray-900">
+                  No upcoming tournaments.
+                </dt>
+              </div>
+            </dl>
+          </div>
+        </div>
+      </div>
     </Layout>
   )
 }
