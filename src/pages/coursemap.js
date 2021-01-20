f274d0398cd9ab552cfd12523eecfc75818f1d23
jsgraef <jg@joshgraef.com>
2021-01-19 20:11:24 -0500

    Add course map

--- src/pages/coursemap.js
+++ src/pages/coursemap.js
@@ -1,10 +1,11 @@
 import React from "react"
 import Layout from "../Layout.js"
+import map from "../images/coursemap.jpg"
 
 export default function CourseMap() {
   return (
     <Layout>
-      <div>This is the map</div>
+      <img src={map} alt="Course Map" />
     </Layout>
   )
 }
